37c39c0fbb9c76e5ab7d57c5762f5034524ccea8
Michael Caley <mjcaley@darkarctic.com>
2016-10-31 21:26:56 -0400

    Week 2 - Reconstruct A String From Its Burrows-Wheeler Transform completed

--- Week 2/ReconstructAStringFromItsBurrows-WheelerTransform/main.cpp
+++ Week 2/ReconstructAStringFromItsBurrows-WheelerTransform/main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -9,12 +10,84 @@ using std::endl;
 using std::string;
 using std::vector;
 
-string InverseBWT(const string& bwt) {
-    string text = "";
+
+class Counter
+{
+public:
+    int add_char(char c)
+    {
+        return ++counter[c];
+    };
     
-    // write your code here
+    int get_count(char c) const
+    {
+        try
+        {
+            return counter.at(c);
+        }
+        catch (std::out_of_range)
+        {
+            return 0;
+        }
+    };
     
-    return text;
+    int count_up_to(char c, int occurance) const
+    {
+        int total { 0 };
+        
+        for (auto& v : counter)
+        {
+            if (c == v.first)
+            {
+                total += occurance;
+                break;
+            }
+            total += v.second;
+        }
+        
+        return --total;
+    };
+    
+private:
+    std::map<char, int> counter;
+};
+
+struct Character
+{
+    Character(char character, int occurance) :
+        character(character), occurance(occurance) {};
+    
+    char character;
+    int occurance;
+};
+
+int row_number(const Character& c, const Counter& counter)
+{
+    return counter.count_up_to(c.character, c.occurance);
+}
+
+string InverseBWT(const string& text) {
+    Counter counter;
+    
+    vector<Character> last_row;
+    for (auto c { text.begin() }; c != text.end(); ++c)
+    {
+        counter.add_char(*c);
+        last_row.emplace_back( Character (*c, counter.get_count(*c)) );
+    }
+    
+    vector<char> result;
+    int index { 0 };
+    for (int i { 0 }; i < last_row.size() - 1; ++i)
+    {
+        result.emplace_back(last_row[index].character);
+        index = row_number(last_row[index], counter);
+    }
+    
+    std::reverse(result.begin(), result.end());
+    result.emplace_back('$');
+    
+    return string(result.begin(), result.end());
 }
 
 int main() {
