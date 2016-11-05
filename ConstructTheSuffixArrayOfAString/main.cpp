41fb7a841a1fcdee744c2786ce0993dc57c681b0
Michael Caley <mjcaley@darkarctic.com>
2016-11-05 19:23:43 -0400

    Map based Suffix Tree

---
+++ ConstructTheSuffixArrayOfAString/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ConstructTheSuffixArrayOfAString
+//
+//  Created by Mike Caley on 2016-11-05.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ FindTheShortestNonSharedSubstringOfTwoStrings/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FindTheShortestNonSharedSubstringOfTwoStrings
+//
+//  Created by Mike Caley on 2016-11-04.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
